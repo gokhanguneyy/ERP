5e6c7b883fb4b7df5cba6e4c294e6b301f91ada3
gokhanguneyy <101227317+gokhanguneyy@users.noreply.github.com>
2025-11-28 16:54:06 +0300

    Create Customer (Frontend)

---
+++ ERPServer/ERPServer.Application/Features/Customers/GetAllCustomer/GetAllCustomerQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Customers.GetAllCustomer
+{
+    internal sealed class GetAllCustomerQueryHandler(ICustomerRepository customerRepository) : IRequestHandler<GetAllCustomerQuery, Result<List<Customer>>>
+    {
+        public async Task<Result<List<Customer>>> Handle(GetAllCustomerQuery request, CancellationToken cancellationToken)
+        {
+            // orderBy ile gelen verileri isme göre sıralıyoruz.
+            List<Customer> customers = await customerRepository.GetAll().OrderBy(p=>p.Name).ToListAsync(cancellationToken);
+            return customers;
+        }
+    }
+}
