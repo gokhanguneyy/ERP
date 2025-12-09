a8af423aebf3a01e7d416290ab7e3e8e8fb95d6e
gokhanguneyy <101227317+gokhanguneyy@users.noreply.github.com>
2025-12-09 12:53:32 +0300

    Recipe ve RecipeDetail modeli oluşturma

---
+++ ERPServer/ERPServer.Domain/Entities/RecipeDetail.cs
@@ -0,0 +1,17 @@
+﻿using ERPServer.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPServer.Domain.Entities
+{
+    public sealed class RecipeDetail : Entity
+    {
+        public Guid RecipeId { get; set; }
+        public Guid ProductId { get; set; }
+        public Product? product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
---
+++ ERPServer/ERPServer.Infrastructure/Repositories/RecipeDetailRepository.cs
@@ -0,0 +1,20 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPServer.Infrastructure.Repositories
+{
+    internal sealed class RecipeDetailRepository : Repository<RecipeDetail, ApplicationDbContext>, IRecipeDetailRepository
+    {
+        public RecipeDetailRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
