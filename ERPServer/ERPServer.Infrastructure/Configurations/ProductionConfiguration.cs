d6e2959d5886aa0a43eb7f4a8ef27ccd7c2aedb5
gokhanguneyy <101227317+gokhanguneyy@users.noreply.github.com>
2025-12-16 21:19:23 +0300

    Production modeli oluşturma

---
+++ ERPServer/ERPServer.Infrastructure/Configurations/ProductionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPServer.Infrastructure.Configurations
+{
+    internal sealed class ProductionConfiguration : IEntityTypeConfiguration<Production>
+    {
+        public void Configure(EntityTypeBuilder<Production> builder)
+        {
+            builder.Property(p => p.Quantity).HasColumnType("decimal(7,2)");
+        }
+    }
+}
