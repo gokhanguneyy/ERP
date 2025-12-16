71b51d363252cb15f0476d0e34a2e81ccc65878c
gokhanguneyy <101227317+gokhanguneyy@users.noreply.github.com>
2025-12-16 22:18:31 +0300

    Stok adedini stok sayfamızda görelim

--- ERPServer/ERPServer.Application/Features/Products/GetAllProduct/GetAllProductQuery.cs
+++ ERPServer/ERPServer.Application/Features/Products/GetAllProduct/GetAllProductQuery.cs
@@ -9,4 +9,4 @@ using TS.Result;
 
 namespace ERPServer.Application.Features.Products.GetAllProduct;
 
-public sealed record GetAllProductQuery() : IRequest<Result<List<Product>>>;
+public sealed record GetAllProductQuery() : IRequest<Result<List<GetAllProductQueryResponse>>>;
---
+++ ERPServer/ERPServer.Application/Features/Products/GetAllProduct/GetAllProductQueryResponse.cs
@@ -0,0 +1,17 @@
+﻿using ERPServer.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPServer.Application.Features.Products.GetAllProduct
+{
+    public sealed record GetAllProductQueryResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ProductTypeEnum Type { get; set; } = ProductTypeEnum.Product;
+        public decimal Stock { get; set; }
+    }
+}
