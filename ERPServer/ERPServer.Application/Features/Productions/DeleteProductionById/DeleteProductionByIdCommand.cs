250e5cb2a4996491b28a4e46e915a86a554b9252
gokhanguneyy <101227317+gokhanguneyy@users.noreply.github.com>
2025-12-16 21:31:23 +0300

    Production CRUD işlemleri (Backend)

---
+++ ERPServer/ERPServer.Application/Features/Productions/DeleteProductionById/DeleteProductionByIdCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Productions.DeleteProductionById
+{
+    public sealed record DeleteProductionByIdCommand(Guid Id) : IRequest<Result<string>>;
+}
